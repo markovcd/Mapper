b05764bdb115f379a527bb35489442901870216a
markovcd <markovcd@gmail.com>
2016-02-10 20:19:33 +0100

    Changed folder structure

---
+++ Entities/Mapping/FormulaMapping.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace Mapper
+{
+	public class FormulaMapping : Mapping
+    {
+        [XmlAttribute]
+        public string Formula { get; set; }
+
+        public string GetValue()
+        {
+            return Formula;
+        }
+    }
+}
