5027538d7f9c631d37e0809115c9720972bacc5a
markovcd <markovcd@gmail.com>
2016-02-16 18:59:29 +0100

    sddfgdfg

---
+++ MapperWPF/BrowseControl.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Microsoft.WindowsAPICodePack;
+
+namespace MapperWPF
+{
+    public class BrowseStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString()
+                        .Split(',')
+                        .Select(v => (BrowseStyle) Enum.Parse(typeof (BrowseStyle), v, true))
+                        .Aggregate((e1, e2) => e1 | e2);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [Flags]
+    public enum BrowseStyle
+    {
+        File = 1, Directory = 2, Open = 4, Save = 8
+    }
+
+    /// <summary>
+    /// Interaction logic for BrowseControl.xaml
+    /// </summary>
+    public partial class BrowseControl : UserControl
+    {
+        public BrowseControl()
+        {
+            InitializeComponent();
+        }
+
+        public BrowseStyle BrowseStyle { get; set; }
+        public string Title { get; set; }
+        public string Filters { get; set; }
+
+        public string Path
+        {
+            get { return FileText.Text; }
+            set { FileText.Text = value; }
+        }
+
+        public static readonly DependencyProperty BrowseStyleProperty =
+          DependencyProperty.Register("BrowseStyle", typeof(BrowseStyle), typeof(BrowseControl));
+
+        public static readonly DependencyProperty TitleProperty =
+          DependencyProperty.Register("Title", typeof(string), typeof(BrowseControl));
+
+        public static readonly DependencyProperty FiltersProperty =
+          DependencyProperty.Register("Filters", typeof(string), typeof(BrowseControl));
+
+        public static readonly DependencyProperty PathProperty =
+          DependencyProperty.Register("Path", typeof(string), typeof(BrowseControl));
+
+        private IEnumerable<CommonFileDialogFilter> ParseFilters()
+        {
+            var split = Filters.Split('|');
+
+            for (var i = 0; i < split.Length - 1; i += 2)
+            {
+                yield return new CommonFileDialogFilter(split[i], split[i + 1]);
+            }
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            var d = CreateFileDialog();
+            if (d.ShowDialog() == CommonFileDialogResult.Cancel) return;
+            Path = d.FileName;
+        }
+
+        private CommonFileDialog CreateFileDialog()
+        {
+            var isDirectory = BrowseStyle.HasFlag(BrowseStyle.Directory);
+            var isSave = BrowseStyle.HasFlag(BrowseStyle.Save);
+            var isOpen = BrowseStyle.HasFlag(BrowseStyle.Open);
+
+            if (isSave && isDirectory) throw new InvalidOperationException();
+            CommonFileDialog d = null;
+
+            if (isSave) d = new CommonSaveFileDialog
+            {
+                EnsureValidNames = true
+            };
+            if (isOpen) d = new CommonOpenFileDialog
+            {
+                IsFolderPicker = isDirectory,
+                EnsureFileExists = true,
+
+            };
+
+            if (d == null) throw new InvalidOperationException();
+
+            d.Title = Title;
+            foreach (var f in ParseFilters()) d.Filters.Add(f);
+
+            return d;
+        }
+    }
+}
---
+++ MapperWPF/ConfigControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace MapperWPF
+{
+    /// <summary>
+    /// Interaction logic for ConfigControl.xaml
+    /// </summary>
+    public partial class ConfigControl : UserControl
+    {
+        public ConfigControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var d = new OpenFileDialog
+            {
+                ValidateNames = true,
+                Multiselect = false,
+                ShowReadOnly = false,
+                CheckFileExists = true
+            };
+
+            d.ShowDialog();
+
+        }
+    }
+}
